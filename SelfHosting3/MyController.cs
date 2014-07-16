d9ac0b89c614e7209b138f35bae7b7864c7ec8d8
Randel Ramirez <randel1_23@hotmail.com>
2014-07-17 01:40:40 +0800

    Updated self-hosting 3

---
+++ SelfHosting3/MyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHost3
+{
+    public class MyController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello from Controller in Self-Hosting3";
+        }
+    }
+}
--- SelfHosting3/Program.cs
+++ SelfHosting3/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using System.Net.Http;
 
 namespace SelfHosting3
 {
@@ -10,6 +13,34 @@ namespace SelfHosting3
     {
         static void Main(string[] args)
         {
+            var config = new HttpSelfHostConfiguration("http://localhost:8999");
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // using asp.net like hosting
+            // HttpSelfHostServer is using a different constructor compared to the previous project (SelfHost1)
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server is up and running");
+            Console.WriteLine("Hit enter to call server with client");
+            Console.ReadLine();
+
+            // HttpSelfHostServer, derives from HttpMessageHandler => multiple level of inheritance
+            var client = new HttpClient(server);
+            client.GetAsync("http://localhost:8999/api/my").ContinueWith(t =>
+            {
+                var result = t.Result;
+                result.Content.ReadAsStringAsync()
+                    .ContinueWith(rt =>
+                    {
+                        Console.WriteLine("client got response" + rt.Result);
+                    });
+            });
+
+            Console.ReadLine();
         }
     }
 }
